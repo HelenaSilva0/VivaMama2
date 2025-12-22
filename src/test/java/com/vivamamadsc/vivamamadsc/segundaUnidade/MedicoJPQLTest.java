e2712b375e47a78a94b1b362b6a0b756baf4ea3c
Neto-Pereira25 <devneto203@gmail.com>
2025-12-21 22:35:10 -0300

    feat: criado teste de buscar por id e buscar todos com jpql na entidade medico

---
+++ src/test/java/com/vivamamadsc/vivamamadsc/segundaUnidade/MedicoJPQLTest.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.vivamamadsc.vivamamadsc.segundaUnidade;
+
+import com.vivamamadsc.vivamamadsc.Medico;
+import com.vivamamadsc.vivamamadsc.primeiraUnidade.DbUnitUtil;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import java.util.List;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertNotNull;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author Neto Pereira
+ */
+public class MedicoJPQLTest {
+
+    private static EntityManagerFactory emf;
+    private static EntityManager em;
+    private EntityTransaction et;
+
+    @BeforeClass
+    public static void setUpClass() {
+        emf = Persistence.createEntityManagerFactory("vivamamadsc");
+        DbUnitUtil.inserirDados();
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        if (emf != null && emf.isOpen()) {
+            emf.close();
+        }
+    }
+
+    @Before
+    public void setUp() {
+        em = emf.createEntityManager();
+        et = em.getTransaction();
+        et.begin();
+    }
+
+    @After
+    public void tearDown() {
+        if (!et.getRollbackOnly()) {
+            et.commit();
+        }
+        em.close();
+    }
+
+    @Test
+    public void testBuscarMedicoPorIdJPQL() {
+        Medico medico = em.createQuery(
+                "SELECT m FROM Medico m WHERE m.id = :id", Medico.class)
+                .setParameter("id", 2L)
+                .getSingleResult();
+
+        assertNotNull(medico);
+        assertEquals("Fulano da Silva", medico.getNome());
+    }
+
+    @Test
+    public void testBuscarTodosMedicos() {
+        List<Medico> medicos = em.createQuery(
+                "SELECT m FROM Medico m", Medico.class
+        ).getResultList();
+
+        assertFalse(medicos.isEmpty());
+
+    }
+
+}
